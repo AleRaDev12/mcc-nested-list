ebed289ff37bed68d17f3956bd3206af34672b4a
AleRaDev12 <aleradev12@gmail.com>
2022-05-16 15:08:23 +0700

    refactor: reformat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    function ({ addVariant }) {
-      addVariant('child', '& > *');
-      addVariant('child-hover', '& > *:hover');
-    }
-  ],
+	content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		function({addVariant}) {
+			addVariant('child', '& > *')
+			addVariant('child-hover', '& > *:hover')
+		},
+	],
 }
